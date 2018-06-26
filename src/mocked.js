cf27dc9c5fe56ba59b03d0408998c81defe8865c
lilei <wongwonglilei@gmail.com>
2018-06-26 16:12:04 +0800

    modify mocked.js

--- src/mocked.js
+++ src/mocked.js
@@ -2,6 +2,7 @@ import fetchMock from "fetch-mock";
 
 const mocked=function(){
     fetchMock.get('*', {
+        "token":"123456",
         "name": "brickspert",
         "intro": "please give me a star"
     });
