5095a954691a88e57ef2933c280d2bafc7856e67
lilei <wongwonglilei@gmail.com>
2018-06-27 11:29:20 +0800

    rename mocked.js to mock.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import {Provider} from 'react-redux';
 import store from './redux/store';
-import mocked from "./mocked";
-mocked();
+import mock from "./mock";
+mock();
 
 
 
--- src/mock.js
+++ src/mock.js
@@ -1,7 +1,7 @@
 import fetchMock from "fetch-mock";
 
 const mocked=function(){
-    fetchMock.get('*', {
+    fetchMock.get('/api/user.json', {
         "token":"1234567",
         "name": "brickspert",
         "intro": "please give me a star"
